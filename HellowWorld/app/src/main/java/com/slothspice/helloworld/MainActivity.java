e15a2ca51e4531202f31760b67577842cdc51e3c
LUO ZHAOWEN <ZHAOWENL3072@bigfoot.spokane.edu>
2018-01-23 10:20:43 -0800

    some work
    yup

--- HellowWorld/app/src/main/java/com/slothspice/helloworld/MainActivity.java
+++ HellowWorld/app/src/main/java/com/slothspice/helloworld/MainActivity.java
@@ -8,45 +8,75 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1;
-    Button btn2;
+
+    Button mul;
+    Button div;
+    Button add;
+    Button min;
     EditText editResult;
-    EditText multiplier;
+    EditText number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn1 = findViewById( R.id.btn1);
-        btn2 = findViewById( R.id.btn2);
+
+        mul = findViewById( R.id.btnMul);
+        div = findViewById( R.id.btnDiv);
+        min = findViewById( R.id.btnMin);
+        add = findViewById( R.id.btnAdd);
         editResult = findViewById( R.id.result);
-        multiplier = findViewById( R.id.result2);
+        number = findViewById( R.id.result2);
 
     }
 
-    public void btnOnClick(View v){
-
-//        if (btn1.getText() == "Jack"){
-//            btn1.setText("Hello");
-//        }else{
-//            btn1.setText("Jack");
-//        }
-
-        //((Button)v).setText("Jack");
-        Double multiplier = Double.parseDouble(this.multiplier.getText().toString());
-        Double currentNumber = Double.parseDouble(editResult.getText().toString());
-        editResult.setText(Double.toString(currentNumber+1));
-
-    }
+//    public void btnOnClick(View v){
+//
+////        if (btn1.getText() == "Jack"){
+////            btn1.setText("Hello");
+////        }else{
+////            btn1.setText("Jack");
+////        }
+//
+//        //((Button)v).setText("Jack");
+//        Double multiplier = Double.parseDouble(this.number.getText().toString());
+//        Double currentNumber = Double.parseDouble(editResult.getText().toString());
+//        editResult.setText(Double.toString(currentNumber+1));
+//
+//    }
 
     public void btnMul(View v){
 
-        Double multiplier = Double.parseDouble(this.multiplier.getText().toString());
+        Double multiplier = Double.parseDouble(this.number.getText().toString());
         Double currentNumber = Double.parseDouble(editResult.getText().toString());
         editResult.setText(Double.toString(currentNumber*multiplier));
 
     }
 
+
+    public void btnDiv(View v){
+
+        Double multiplier = Double.parseDouble(this.number.getText().toString());
+        Double currentNumber = Double.parseDouble(editResult.getText().toString());
+        editResult.setText(Double.toString(currentNumber/multiplier));
+
+    }
+
+    public void btnMin(View v){
+
+        Double multiplier = Double.parseDouble(this.number.getText().toString());
+        Double currentNumber = Double.parseDouble(editResult.getText().toString());
+        editResult.setText(Double.toString(currentNumber-multiplier));
+
+    }
+
+    public void btnAdd(View v){
+
+        Double multiplier = Double.parseDouble(this.number.getText().toString());
+        Double currentNumber = Double.parseDouble(editResult.getText().toString());
+        editResult.setText(Double.toString(currentNumber+multiplier));
+
+    }
 
 
 
