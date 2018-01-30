c4353232e276df2220e456daf13a967344fa8020
Jack Luo <jackluo94@hotmail.com>
2018-01-30 11:24:02 -0800

    sdgsd
    gsdgsd

--- Calculator/app/src/main/java/com/jack/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/jack/calculator/MainActivity.java
@@ -7,11 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.lang.Math;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText results;
     Button btnPlus, btnMult, btnMinus, btnDiv, btnEquals;
+    Button btnSq, btnSqR, btnPercent, btnCos, btnSin, btnTan, btnBack, btnPi, btnChange, btnRand;
 
     int pendingOperation;
     double previousResult, currentValue, operand;
@@ -27,12 +29,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         results = (EditText) findViewById(R.id.resultText);
         results.setFocusable(false);
-        //results.setText(Double.toString(5.0-17.24));
+
+
         btnPlus = (Button) findViewById(R.id.btnPlus);
         btnMinus = (Button) findViewById(R.id.btnMinus);
         btnMult = (Button) findViewById(R.id.btnMult);
         btnDiv = (Button) findViewById(R.id.btnDiv);
         btnEquals = (Button) findViewById(R.id.btnEquals);
+
+
+        btnBack = (Button) findViewById(R.id.btnBack);
+        btnChange = (Button) findViewById(R.id.btnChange);
+        btnTan = (Button) findViewById(R.id.btnTan);
+        btnSin = (Button) findViewById(R.id.btnSin);
+        btnCos = (Button) findViewById(R.id.btnCos);
+        btnPercent = (Button) findViewById(R.id.btnPercent);
+        btnSqR = (Button) findViewById(R.id.btnSqR);
+        btnRand = (Button) findViewById(R.id.btnRand);
+        btnPi = (Button) findViewById(R.id.btnPi);
+        btnSq = (Button) findViewById(R.id.btnSq);
     }
 
     public void btnEqualsOnClick(View v) {
@@ -73,6 +88,89 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
+    }
+
+    public void btnMathOperationOnClick(View v) {
+        int mathOperation = ((Button) v).getId();
+        double currentNumber;
+
+        if (newInput) {
+            newInput = false;
+        }
+
+        if(mathOperation == btnRand.getId()) {
+            results.setText(Double.toString(Math.random()));
+        } else if (mathOperation == btnBack.getId()) {
+
+            if(results.getText().toString().toLowerCase().equals("infinity")){
+                results.setText("0");
+            } else if((results.getText().toString().length() > 1 && Double.parseDouble(results.getText().toString()) > 0) ||
+                    (results.getText().toString().length() > 2 && Double.parseDouble(results.getText().toString()) < 0)) {
+
+                if (results.getText().toString().toLowerCase().contains("e") ) {
+
+                    String[] split = results.getText().toString().toLowerCase().split("e");
+                    String remake;
+                    if (Integer.parseInt(split[1]) > 0) {
+                        remake = split[0] + "E" + Integer.toString(Integer.parseInt(split[1]) - 1);
+                    }else{
+                        remake = split[0] + "E" + Integer.toString(Integer.parseInt(split[1]) + 1);
+                    }
+
+                    double rdb = Double.parseDouble(remake);
+                    results.setText(Double.toString(rdb));
+
+                }else{
+                    results.setText(results.getText().toString().substring(0, results.getText().toString().length() - 1));
+                }
+
+            }else {
+                results.setText("0");
+            }
+
+        } else if (mathOperation == btnChange.getId()) {
+
+            if(Double.parseDouble(results.getText().toString()) > 0){
+                currentNumber = Double.parseDouble(results.getText().toString());
+                results.setText(Double.toString(currentNumber * -1));
+            }else {
+                currentNumber = Double.parseDouble(results.getText().toString());
+                results.setText(Double.toString(currentNumber * -1));
+            }
+
+        }else if (mathOperation == btnSq.getId()) {
+
+            currentNumber = Double.parseDouble(results.getText().toString());
+            results.setText(Double.toString(currentNumber * currentNumber));
+
+        }else if (mathOperation == btnPi.getId()) {
+
+            results.setText(Double.toString(Math.PI));
+
+        }else if (mathOperation == btnSqR.getId()) {
+            currentNumber = Double.parseDouble(results.getText().toString());
+            results.setText(Double.toString(Math.sqrt(currentNumber)));
+
+        }else if (mathOperation == btnPercent.getId()) {
+            currentNumber = Double.parseDouble(results.getText().toString());
+            results.setText(Double.toString(currentNumber / 100.0));
+        }
+        else if (mathOperation == btnCos.getId()) {
+            currentNumber = Double.parseDouble(results.getText().toString());
+            results.setText(Double.toString(Math.cos(Math.toRadians(currentNumber))));
+
+        }else if (mathOperation == btnSin.getId()) {
+            currentNumber = Double.parseDouble(results.getText().toString());
+            results.setText(Double.toString(Math.sin(Math.toRadians(currentNumber))));
+        }else if (mathOperation == btnTan.getId()) {
+            currentNumber = Double.parseDouble(results.getText().toString());
+            results.setText(Double.toString(Math.tan(Math.toRadians(currentNumber))));
+        }
+
+        equalClicked = true;
+//        previousResult = 0;
+//        pendingOperation = 0;
+        operand = Double.parseDouble(results.getText().toString());
     }
 
 
@@ -139,6 +237,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+
+
+
     public void btnNumberOnClick(View v) {
 
         if (equalClicked) {
@@ -181,9 +283,7 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-
         operand = Double.parseDouble(results.getText().toString());
-
 
     }
 
